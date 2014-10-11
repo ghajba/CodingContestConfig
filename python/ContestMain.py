e05028180a1e1fc8f2b2f96eaf42d98215e63c62
Gabor <gabor.hajba@gmail.com>
2014-10-11 11:06:00 +0200

    made the main program more functional

--- python/ContestMain.py
+++ python/ContestMain.py
@@ -1,7 +1,12 @@
 import os
+from glob import glob
 from filecmp import cmp
 
 from ContestCode import dummy_message
+from ContestCode import do_level_1
+from ContestCode import do_level_2
+from ContestCode import do_level_3
+from ContestCode import do_level_4
 
 
 PATH = "."
@@ -9,10 +14,14 @@ FOLDER_PREFIX = "level"
 DEFAULT_FOLDER = PATH + '/' + FOLDER_PREFIX
 
 
-def write_result(some_result_value, output_location, output_file_name):
+def write_result_to_location(some_result_value, output_location, output_file_name):
     """ Writes a result value to the given file at the given location!"""
-    with open(output_location + '/' + output_file_name, 'w') as content_file:
-        content_file.write(some_result_value)
+    write_result(some_result_value, output_location + '/' + output_file_name)
+
+
+def write_result(result_value, output_file_name):
+    with open(output_file_name, 'w') as content_file:
+        content_file.write(result_value)
 
 
 def read_input_file_from_location(input_location, input_file_name):
@@ -49,36 +58,31 @@ def compare_file_with_content(file_with_path, expected_content):
 def do_setup_testing():
     """ tests the setup of the notebook """
     folder_name = create_default_folder('0')
-    write_result("foo", folder_name, 'testInput.txt')
+    write_result_to_location("foo", folder_name, 'testInput.txt')
     input_content = read_input_file_from_location(folder_name, 'testInput.txt')
-    write_result(dummy_message(input_content), folder_name, 'testOutput.txt')
+    write_result_to_location(dummy_message(input_content), folder_name, 'testOutput.txt')
     print compare_file_contents(folder_name + '/testInput.txt', folder_name + '/testOutput.txt')
     print compare_file_with_content(folder_name + '/testOutput.txt', dummy_message(input_content))
+    do_it_nasty(folder_name, False, dummy_message)
+    do_it_nasty(folder_name, True, dummy_message)
 
 
-def do_level_1():
-    """ Calls the required methods for level 1 of the contest. It should stay the same along the later levels. """
-    folder_name = create_default_folder('1')
-
-
-def do_level_2():
-    """ Calls the required methods for level 2 of the contest. It should stay the same along the later levels. """
-    folder_name = create_default_folder('2')
-
-
-def do_level_3():
-    """ Calls the required methods for level 3 of the contest. It should stay the same along the later levels. """
-    folder_name = create_default_folder('3')
-
-
-def do_level_4():
-    """ Calls the required methods for level 4 of the contest. It should stay the same along the later levels. """
-    folder_name = create_default_folder('4')
+def do_it_nasty(folder_name, level_completed, level_function):
+    create_folder(folder_name)
+    for content_file in glob(folder_name + '/' + "*.in"):
+        input_content = read_input_file(content_file)
+        result = level_function(input_content)
+        out_file_name = content_file.replace(".in", ".out")
+        if not level_completed:
+            write_result(result, out_file_name)
+        else:
+            expected = read_input_file(out_file_name)
+            assert expected == result, "Actual result [%s] is not as expected [%s]!" % (result, expected)
 
 
 if __name__ == '__main__':
     do_setup_testing()
-    do_level_1()
-    do_level_2()
-    do_level_3()
-    do_level_4()
+    do_it_nasty("level1", False, do_level_1)
+    do_it_nasty("level2", False, do_level_2)
+    do_it_nasty("level3", False, do_level_3)
+    do_it_nasty("level4", False, do_level_4)
